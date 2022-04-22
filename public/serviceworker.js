acc15ae4d76be9e835ad1c351501d06bff9b5cf8
Dom Christie <christiedom@gmail.com>
2022-04-22 15:35:21 -0700

    Scaffold serviceworker

---
+++ public/serviceworker.js
@@ -0,0 +1,34 @@
+console.log('[Service Worker] Hello world!')
+
+var CACHE_NAME = 'v1-cached-assets'
+
+function onInstall (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function prefill (cache) {
+      return cache.addAll([
+        // files to cache
+      ])
+    })
+  )
+}
+
+function onActivate (event) {
+  console.log('[Serviceworker]', "Activating!", event)
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function (cacheName) {
+          // Return true if you want to remove this cache,
+          // but remember that caches are shared across
+          // the whole origin
+           return cacheName.indexOf('v1') !== 0
+        }).map(function (cacheName) {
+          return caches.delete(cacheName)
+        })
+      )
+    })
+  )
+}
+
+self.addEventListener('install', onInstall)
+self.addEventListener('activate', onActivate)
