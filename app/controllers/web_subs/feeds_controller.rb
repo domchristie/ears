967647a1749f5daedf1a0fb439d2f32359d7436c
Dom Christie <christiedom@gmail.com>
2022-04-26 13:30:30 -0700

    Debug feeds not syncing from web subs

--- app/controllers/web_subs/feeds_controller.rb
+++ app/controllers/web_subs/feeds_controller.rb
@@ -1,4 +1,6 @@
 class WebSubs::FeedsController < ApplicationController
+  class InvalidSignature < StandardError; end
+
   skip_forgery_protection only: :update
 
   def update
@@ -6,6 +8,8 @@ class WebSubs::FeedsController < ApplicationController
 
     if subscription.validate_signature(*parse_signature_header)
       SyncFeedJob.perform_later(subscription.feed, source: :web_sub)
+    else
+      raise InvalidSignature.new(request.headers["X-Hub-Signature"])
     end
 
     head :ok
