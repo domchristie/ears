6c83be389b5336f77fceab98a13df91c08b135c0
Dom Christie <christiedom@gmail.com>
2022-04-30 15:33:22 -0700

    Improve play remaining resolution

--- app/helpers/plays_helper.rb
+++ app/helpers/plays_helper.rb
@@ -1,2 +1,14 @@
 module PlaysHelper
+  def play_remaining_in_words(remaining)
+    if remaining > 0
+      remaining = ActiveSupport::Duration.build(remaining.round)
+      parts = remaining.parts
+      [
+        (parts[:hours].to_i > 1 && "#{parts[:hours]} hrs"),
+        (parts[:hours].to_i == 1 && "#{parts[:hours]} hr"),
+        (parts[:minutes].to_i > 1 && "#{parts[:minutes]} mins"),
+        (parts[:minutes].to_i == 1 && "#{parts[:minutes]} min")
+      ].select(&:itself).join(" ")
+    end
+  end
 end
