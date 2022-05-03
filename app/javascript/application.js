cb5e6751c79a0ceabf26eecd077876383194d92e
Dom Christie <christiedom@gmail.com>
2022-05-02 20:25:23 -0700

    Keep dashboard up-to-date when navigating back

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,19 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import LocalTime from "local-time"
 LocalTime.start()
+
+
+// clear cache after both turbo-frame and turbo-stream form submissions
+// https://github.com/hotwired/turbo/issues/554#issuecomment-1078479296
+;(function () {
+const FetchMethod = { get: 0 }
+  document.addEventListener('turbo:submit-end', async ({ detail }) => {
+    console.log("turbo:submit-end")
+    const nonGetFetch = detail.formSubmission.fetchRequest.method !== FetchMethod.get
+    const responseHTML = await detail.fetchResponse.responseHTML
+    if (detail.success && nonGetFetch && responseHTML) {
+      console.log("clearing")
+      Turbo.clearCache()
+    }
+  })
+})()
