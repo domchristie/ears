85bdbbf76b3786c5dd2c8461c87323c2c3d576f9
Dom Christie <christiedom@gmail.com>
2022-05-05 10:15:58 -0700

    Improve invalid signature debug

--- app/controllers/web_subs/feeds_controller.rb
+++ app/controllers/web_subs/feeds_controller.rb
@@ -1,5 +1,14 @@
 class WebSubs::FeedsController < ApplicationController
-  class InvalidSignature < StandardError; end
+  class InvalidSignature < StandardError
+    def initialize(message, context)
+      @context = context
+      super(message)
+    end
+
+    def to_honeybadger_context
+      @context
+    end
+  end
 
   skip_forgery_protection only: :update
 
@@ -7,10 +16,14 @@ class WebSubs::FeedsController < ApplicationController
     subscription = WebSub.find(params[:web_sub_id])
     algorithm, signature = parse_signature_header
 
-    if subscription.validate_signature(algorithm, signature, request.body.read)
+    body = request.body.read
+    if subscription.validate_signature(algorithm, signature, body)
       SyncFeedJob.perform_later(subscription.feed, source: :web_sub)
     else
-      raise InvalidSignature.new(request.headers["X-Hub-Signature"])
+      raise InvalidSignature.new(
+        "InvalidSignature",
+        algorithm: algorithm, signature: signature, body: body
+      )
     end
 
     head :ok
