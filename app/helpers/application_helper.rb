5dc9f80b879fedb9707eda2340d54f9088b6f179
Dom Christie <christiedom@gmail.com>
2022-05-04 21:12:44 -0700

    Icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def icon(name, options = {})
+    size = options[:solid] ? 20 : 24
+    options[:width] = options[:height] = size
+    inline_svg_tag("heroicons/#{options.delete(:solid) ? "solid" : "outline"}/#{name}.svg", options)
+  end
 end
