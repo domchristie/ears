3b9dcd6e26439d57c09afd935bef10f78da5e7fd
Dom Christie <christiedom@gmail.com>
2022-05-05 21:55:20 -0700

    Hardware accelerate turn transitions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
     options[:width] = options[:height] = size
     inline_svg_tag("heroicons/#{options.delete(:solid) ? "solid" : "outline"}/#{name}.svg", options)
   end
+
+  def turn_exit_class_names
+    "motion-safe:turn-exit:animate-exit transform-gpu"
+  end
+
+  def turn_enter_class_names
+    "motion-safe:turn-enter:animate-enter transform-gpu"
+  end
 end
