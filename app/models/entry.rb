24ed3b1a53e0a88591b2ad7c59c475262a3daff0
Dom Christie <christiedom@gmail.com>
2022-05-12 11:11:24 -0700

    Prompt to restart play when complete

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -15,7 +15,8 @@ class Entry < ApplicationRecord
   end
 
   def resume_enclosure_url(play)
-    [enclosure_url, play.try(:elapsed)].compact.join("#t=")
+    elapsed = play.try(:complete?) ? nil : play.try(:elapsed)
+    [enclosure_url, elapsed].compact.join("#t=")
   end
 
   def media_session_metadata
