8102ccf618f37b3e69e6eb2127285d857611b74f
Dom Christie <christiedom@gmail.com>
2022-06-02 21:27:52 -0700

    Test opml import flow

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,4 +2,15 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+
+  setup do
+    WebMock.allow_net_connect!
+  end
+
+  def login(user)
+    visit root_path
+    fill_in "Email", with: user.email
+    click_button "Sign In"
+    assert_current_path root_path
+  end
 end
---
+++ test/system/opml_imports_test.rb
@@ -0,0 +1,16 @@
+require "application_system_test_case"
+
+class OpmlImportsTest < ApplicationSystemTestCase
+  test "importing an OPML" do
+    user = users(:one)
+    login user
+    click_link "Settings"
+    click_link "Import OPML"
+    attach_file "Choose OPML file", file_fixture("valid.opml")
+    click_button "Import"
+    assert_text "Import in progess"
+    click_link "Listen"
+    assert_selector "article p a", text: "Radiolab"
+    assert_selector "article p a", text: "99% Invisible"
+  end
+end
