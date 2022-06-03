d2760bd942962df5233145253cd4efb094fbc251
Dom Christie <christiedom@gmail.com>
2022-06-02 21:26:44 -0700

    Remove unused controller tests

--- test/controllers/opml_imports_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class OpmlImportsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/controllers/users/dashboards_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class Users::DashboardsControllerTest < ActionDispatch::IntegrationTest
-  test "should get show" do
-    get users_dashboards_show_url
-    assert_response :success
-  end
-end
