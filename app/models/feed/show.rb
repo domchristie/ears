c7d4e915120b003f7572d1e43e0579980dc337fa
Dom Christie <christiedom@gmail.com>
2022-09-28 17:28:44 +0100

    Tidy Feed::Show actions

--- app/models/feed/show.rb
+++ app/models/feed/show.rb
@@ -1,4 +1,6 @@
 class Feed::Show
+  delegate_missing_to :feed
+
   def initialize(params)
     @params = params
   end
@@ -21,6 +23,13 @@ class Feed::Show
     @feed ||= find_feed
   end
 
+  def actions
+    [
+      (replay_resume_action if most_recent_play.present?),
+      (latest_action if most_recent_play&.entry != most_recent_entry)
+    ].compact
+  end
+
   private
 
   def find_feed
@@ -30,5 +39,35 @@ class Feed::Show
       url = Feed.decode_url(@params[:encoded_url])
       Feed.find_or_create_by(url: url)
     end
+  end
+
+  def most_recent_play
+    @most_recent_play ||= feed.most_recent_play_by(Current.user)
+  end
+
+  def most_recent_entry
+    @most_recent_entry ||= feed.most_recent_entry
+  end
+
+  def replay_resume_action
+    {
+      partial: "plays/featured",
+      locals: {
+        title: most_recent_play.complete? ? "Replay" : "Resume",
+        play: most_recent_play,
+        highlight: @params[:source] == "recently_played"
+      }
+    }
+  end
+
+  def latest_action
+    {
+      partial: "plays/featured",
+      locals: {
+        title: "Latest",
+        play: most_recent_entry.upcoming_play_by(Current.user),
+        highlight: @params[:source] == "recently_updated"
+      }
+    }
   end
 end
