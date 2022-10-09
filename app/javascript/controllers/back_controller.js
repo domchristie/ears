e15072f75c54ee2094f56eec78cbb10285112c20
Dom Christie <christiedom@gmail.com>
2022-10-09 13:42:46 -0700

    Animate back

--- app/javascript/controllers/back_controller.js
+++ app/javascript/controllers/back_controller.js
@@ -3,6 +3,13 @@ import { Controller } from '@hotwired/stimulus'
 export default class BackController extends Controller {
   back (event) {
     event.preventDefault()
+    document.body.dataset.turnOptions = JSON.stringify({
+      animateRestore: true
+    })
     window.history.back()
+  }
+
+  disconnect () {
+    delete document.body.dataset.turnOptions
   }
 }
