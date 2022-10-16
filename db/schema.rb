e87813831bd3e6d8b7d5d09cbb8c802de1cfc479
Dom Christie <christiedom@gmail.com>
2022-10-16 16:47:00 -0700

    Install pg extensions for pg_search

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_02_174046) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_16_234459) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "fuzzystrmatch"
+  enable_extension "pg_trgm"
   enable_extension "plpgsql"
 
   create_table "active_sessions", force: :cascade do |t|
