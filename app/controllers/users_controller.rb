6a7c009e75a544c1913498ff217ecf377c759a79
Dom Christie <christiedom@gmail.com>
2022-10-28 11:39:46 -0700

    Setup confirmations mailer flow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      @user.send_confirmation_email!
       login_and_remember @user
       redirect_to root_path, notice: "Check your email for confirmation instructions"
     else
