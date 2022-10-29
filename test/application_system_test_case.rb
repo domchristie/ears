9498d1180f9cc8dd6fb60f4b82de7371ac3c65e3
Dom Christie <christiedom@gmail.com>
2022-10-28 19:00:18 -0700

    Prepare for system tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,15 +1,16 @@
 require "test_helper"
 
+WebMock.disable_net_connect!(allow: lambda { |uri|
+  uri.host.include?("chromedriver.storage.googleapis.com")
+})
+
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
-
-  setup do
-    WebMock.allow_net_connect!
-  end
 
   def login(user)
     visit root_path
     fill_in "Email", with: user.email
+    fill_in "Password", with: "password"
     click_button "Sign In"
     assert_current_path root_path
   end
