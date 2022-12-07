6c2cbd7d156687ecbd553ef034dcb03841397f0f
Dom Christie <christiedom@gmail.com>
2022-12-07 10:59:53 +0000

    Add append to Playlist

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,5 +1,10 @@
 class Playlist < ApplicationRecord
   include Hashid::Rails
   belongs_to :user
-  has_many :playlist_items, dependent: :destroy
+  has_many :items, class_name: "PlaylistItem", dependent: :destroy
+  has_many :entries, through: :items
+
+  def append!(entry)
+    items.create!(entry:)
+  end
 end
--- test/models/playlist_test.rb
+++ test/models/playlist_test.rb
@@ -1,7 +1,12 @@
 require "test_helper"
 
 class PlaylistTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "#append! creates a PlaylistItem" do
+    playlist = playlists(:queue)
+    entry = entries(:two)
+    refute playlist.entries.include?(entry)
+
+    assert_difference(-> { PlaylistItem.count }) { playlist.append!(entry) }
+    assert playlist.entries.include?(entry)
+  end
 end
