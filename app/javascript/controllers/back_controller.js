5e090df98405ea4d398dbd1779f601967db89ee5
Dom Christie <christiedom@gmail.com>
2023-08-05 10:24:17 +0100

    Fix restore animations

--- app/javascript/controllers/back_controller.js
+++ app/javascript/controllers/back_controller.js
@@ -3,13 +3,10 @@ import { Controller } from '@hotwired/stimulus'
 export default class BackController extends Controller {
   back (event) {
     event.preventDefault()
-    document.body.dataset.turnOptions = JSON.stringify({
-      animateRestore: true
-    })
+    document.documentElement.dataset.animateRestore = true
+    window.addEventListener('turn:enter', () => {
+      document.documentElement.removeAttribute('data-animate-restore')
+    }, { once: true })
     window.history.back()
-  }
-
-  disconnect () {
-    delete document.body.dataset.turnOptions
   }
 }
