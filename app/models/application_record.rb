aa2f7879daf113b4901ee6ba4d5699f8963d0105
Dom Christie <christiedom@gmail.com>
2023-08-14 09:26:12 +0100

    Obfuscate record ids in dom_id generation

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  def to_key
+    key = (respond_to?(:hashid) && hashid) || (respond_to?(:id) && id)
+    key ? [key] : nil
+  end
 end
