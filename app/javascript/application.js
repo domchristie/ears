41afcff16819916a50c6f44e024e541aae5f85c2
Dom Christie <christiedom@gmail.com>
2023-09-06 18:33:14 +0100

    Remove unused Turbo cache patch

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,19 +4,7 @@ import "controllers"
 import LocalTime from "local-time"
 LocalTime.start()
 
-// clear cache after both turbo-frame and turbo-stream form submissions
-// https://github.com/hotwired/turbo/issues/554#issuecomment-1078479296
 ;(function () {
-  const FetchMethod = { get: 0 }
-  document.addEventListener('turbo:submit-end', async ({ detail }) => {
-    const shouldClearCache = (
-      detail.success &&
-      await detail.fetchResponse?.responseHTML &&
-      detail.formSubmission.fetchRequest.method !== FetchMethod.get
-    )
-    if (shouldClearCache) Turbo.cache.clear()
-  })
-
   // Fix buttons becoming re-enabled during response processing
   let submitter = null
   addEventListener('turbo:submit-end', function (event) {
