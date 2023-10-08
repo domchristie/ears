7e2246774265c73852d83e8abd5012f98f8aa3f9
Dom Christie <christiedom@gmail.com>
2023-10-08 11:23:10 +0100

    Prepare for accurate initial history entry

--- app/javascript/controllers/back_controller.js
+++ app/javascript/controllers/back_controller.js
@@ -1,11 +1,24 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class BackController extends Controller {
-  back () {
+  back (event) {
     document.documentElement.dataset.animateRestore = true
     window.addEventListener('turn:enter', () => {
       document.documentElement.removeAttribute('data-animate-restore')
     }, { once: true })
-    window.history.back()
+
+    if (this.#shouldRestore) {
+      event.preventDefault()
+      window.history.back()
+    }
+  }
+
+  get #shouldRestore () {
+    return !this.#isFirstHistoryEntry
+  }
+
+  get #isFirstHistoryEntry () {
+    return !window.history.state.turbo ||
+      window.history.state.turbo.restorationIndex === 0
   }
 }
