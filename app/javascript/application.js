4dedc9f24da529b5198dd85a4940ac33daa14ee6
Dom Christie <christiedom@gmail.com>
2024-03-03 09:42:58 +0000

    Lint

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-import LocalTime from "local-time"
+import '@hotwired/turbo-rails'
+import 'controllers'
+import LocalTime from 'local-time'
 LocalTime.start()
-
 ;(function () {
   // Fix buttons becoming re-enabled during response processing
   let submitter = null
