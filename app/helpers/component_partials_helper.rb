1d2256f6a7d121b683e7927ccabddee650925fb8
Dom Christie <christiedom@gmail.com>
2024-09-17 07:57:08 +0100

    Improve performance of component partials. Do not use UUID

--- app/helpers/component_partials_helper.rb
+++ app/helpers/component_partials_helper.rb
@@ -6,21 +6,15 @@ module ComponentPartialsHelper
   private
 
   class Partial
-    DEFAULT_CONTENT_FOR_OPTIONS = [:flush]
-
     attr_reader :options
 
     delegate_missing_to :@view_context
 
     def initialize(view_context, options)
       @view_context = view_context
-      @key = SecureRandom.uuid
+      @contents = Hash.new { |h, k| h[k] = ActiveSupport::SafeBuffer.new }
       @options = options
       @content_options = {}
-    end
-
-    def helpers(&block)
-      class_eval &block
     end
 
     def content_for(name, content = nil, options = {}, &block)
@@ -30,31 +24,26 @@ module ComponentPartialsHelper
           content = @view_context.capture(&block)
         end
 
-        options_for(name, options.without(*DEFAULT_CONTENT_FOR_OPTIONS))
-      end
+        options_for(name, options)
 
-      @view_context.content_for(
-        name_for(name),
-        content,
-        options.slice(*DEFAULT_CONTENT_FOR_OPTIONS)
-      )
+        @contents[name] << content.to_s
+        nil
+      else
+        @contents[name].presence
+      end
     end
 
     def content_for?(name)
-      @view_context.content_for?(name_for(name))
+      @contents[name].present?
     end
 
     def options_for(name, options = nil)
       if options
-        @content_options[name_for(name)] = options
+        @content_options[name] = options
         nil
       else
-        @content_options[name_for(name)] || {}
+        @content_options[name] || {}
       end
-    end
-
-    def name_for(name)
-      "#{name}_#{@key}".to_sym
     end
   end
 end
