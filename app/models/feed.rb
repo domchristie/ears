1d54f63bcd65ba2a36e27f3ee166799315119054
Dom Christie <christiedom@gmail.com>
2025-06-04 19:39:38 +0100

    Use WebSub concern to tidy Feed

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,24 +1,17 @@
 class Feed < ApplicationRecord
   include Hashid::Rails
+  include WebSubs
   include Importable
 
   has_many :entries, dependent: :destroy
   has_one :most_recent_entry, -> { order(published_at: :desc) }, class_name: "Entry"
   has_one :rss_image, as: :rss_imageable, dependent: :destroy
-  has_many :web_subs, foreign_key: :feed_url, primary_key: :url, dependent: :destroy
-  has_many :active_web_subs, -> { where("active_web_subs.expires_at > ?", Time.current) }, class_name: "WebSub", foreign_key: :feed_url, primary_key: :url
   has_many :plays, dependent: :destroy
   has_many :followings, dependent: :destroy
   has_one :following
   has_many :playlist_items, through: :entries
 
   scope :followed_by, ->(user) { where(id: user.followed_feeds) }
-  scope :web_subable, -> { Feed.where.not(web_sub_hub_url: nil) }
-
-  before_save if: :will_save_change_to_url? do
-    WebSub.where(feed_url: url_in_database).destroy_all
-  end
-  after_commit :start_web_sub, if: :web_sub_attributes_changed?
 
   validates :url, format: %r{http(s)?://.+}
 
@@ -65,17 +58,5 @@ class Feed < ApplicationRecord
 
   def followed_by?(user)
     followings.where(user: user).exists?
-  end
-
-  def web_subable?
-    web_sub_hub_url.present?
-  end
-
-  def web_sub_attributes_changed?
-    saved_change_to_web_sub_hub_url? || saved_change_to_url?
-  end
-
-  def start_web_sub
-    StartWebSubJob.perform_later(self)
   end
 end
---
+++ app/models/feed/web_subs.rb
@@ -0,0 +1,28 @@
+module Feed::WebSubs
+  extend ActiveSupport::Concern
+
+  included do
+    has_many :web_subs, foreign_key: :feed_url, primary_key: :url, dependent: :destroy
+    has_many :active_web_subs, -> { where("active_web_subs.expires_at > ?", Time.current) }, class_name: "WebSub", foreign_key: :feed_url, primary_key: :url
+
+    scope :web_subable, -> { Feed.where.not(web_sub_hub_url: nil) }
+
+    before_save if: :will_save_change_to_url? do
+      WebSub.where(feed_url: url_in_database).destroy_all
+    end
+
+    after_commit :start_web_sub, if: :web_sub_attributes_changed?
+  end
+
+  def web_subable?
+    web_sub_hub_url.present?
+  end
+
+  def web_sub_attributes_changed?
+    saved_change_to_web_sub_hub_url? || saved_change_to_url?
+  end
+
+  def start_web_sub
+    StartWebSubJob.perform_later(self)
+  end
+end
