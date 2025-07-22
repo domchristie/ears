0c7c311fe6efd5d2383010d80ebb95c7f7f40e03
Dom Christie <christiedom@gmail.com>
2025-07-22 20:59:32 +0100

    Prepare for more podcast namespace attrs

--- config/initializers/feedjira.rb
+++ config/initializers/feedjira.rb
@@ -1,9 +1,18 @@
 class Feedjira::Parser::ITunesRSS
   elements :link, as: :hubs, value: :href, with: {rel: "hub"}
   elements :"atom:link", as: :hubs, value: :href, with: {rel: "hub"}
+  element :"podcast:guid", as: :podcast_guid
+  element :"podcast:funding", as: :podcast_funding_text
+  element :"podcast:funding", value: :url, as: :podcast_funding_url
 end
 
 class Feedjira::Parser::ITunesRSSItem
   element :"podcast:chapters", value: :url, as: :podcast_chapters_url
   element :"podcast:chapters", value: :type, as: :podcast_chapters_type
+  element :"podcast:funding", as: :podcast_funding_text
+  element :"podcast:funding", value: :url, as: :podcast_funding_url
+  elements :"podcast:transcript", value: :url, as: :podcast_transcript_urls
+  elements :"podcast:transcript", value: :type, as: :podcast_transcript_types
+  elements :"podcast:transcript", value: :language, as: :podcast_transcript_languages
+  elements :"podcast:transcript", value: :rel, as: :podcast_transcript_rels
 end
